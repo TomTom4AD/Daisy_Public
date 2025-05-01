9626f838d33ff0edb40c23c3d51f7013601dfcb2
TomTom4AD <tomasztomzik@yahoo.de>
2025-05-01 20:00:19 +0200

    pushed

---
+++ Weight_Height_EX.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv('weight-height.csv', delimiter=';')
+print(df)
+print(type(df))
+
+df.Height *= 2,54
+df.Weight *= 2,2
+
+print(df.head(3))
