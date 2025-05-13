7d72f35d3bb6e33f8542909f1d65a89a29bbc2cc
TomTom4AD <tomasztomzik@yahoo.de>
2025-05-13 22:11:31 +0200

    Pushed 13.05.25

--- 5_Weight-Height_EX.py
+++ 5_Weight-Height_EX.py
@@ -1,10 +1,22 @@
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-df = pd.read_csv('weight-height.csv', delimiter = ';')
+df = pd.read_csv('weight-height_spaces.csv')
 print(df)
 
 df.Height *= 2.54
 df.Weight /= 2.2
 print('Po zmianie jednostek')
 print(df.head(10))
+
+#plt.hist(df.Weight)
+#plt.show()
+#sns.histplot(df.Weight)
+#plt.show()
+
+sns.histplot(df.query('Gender=="Male"').Weight)
+plt.show()
+sns.histplot(df.query('Gender=="Female"').Weight)
+plt.show()
