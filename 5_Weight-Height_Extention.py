2446d370fc502643f80c377482447e53508b6e1b
TomTom4AD <tomasztomzik@yahoo.de>
2025-05-23 21:46:32 +0200

    NEW 5_Weight-Height file

--- 5_Weight-Height_Extention.py
+++ 5_Weight-Height_Extention.py
@@ -10,3 +10,24 @@ print(type(df))
 df.Height += 2.54
 df.Weight *= 2.2
 print(df.head(3)) # pierwsze 3 wiersze
+
+#sns.histplot(df.query('Gender=="Male"').Weight)     #drukuje wykres "Weight" dla Gender = Male
+#sns.histplot(df.query('Gender=="Female"').Weight)   #drukuje wykres "Weight" dla Gender = Female
+#plt.show()
+
+#zmiana Gender na dane numeryczne
+df = pd.get_dummies(df)
+del df['Gender_Male']
+df = df.rename(columns={'Gender_Female' : 'Gender'})
+print(df.head())
+# False = facet, True = kobieta
+
+model = LinearRegression()
+model.fit(df[['Height', 'Gender']], df['Weight']) # weź 'Height' i weź 'Gender' i naucz się przez "fit"
+
+# coef_ parametr "a" w funkcji y = ax + b,
+# intercept_ to wyraz wolny b
+print(f'Wspoczynnik kierunkowy: {model.coef_}\nWyraz wolny: {model.intercept_}')
+
+print(f'Weight = Height * {model.coef_[0]} + Gender * {model.coef_[1]} + {model.intercept_}')
+print(model.predict([[160, 1]]))
