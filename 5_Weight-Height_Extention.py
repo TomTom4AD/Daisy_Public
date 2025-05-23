307676b7ae09b3e5661d2c8befb578be6ef5c0cf
TomTom4AD <tomasztomzik@yahoo.de>
2025-05-23 20:19:04 +0200

    NEW 5_Weight-Height file

---
+++ 5_Weight-Height_Extention.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv('weight-height_spaces.csv', delimiter=';')
+print(df)
+print(type(df))
+
+df.Height += 2.54
+df.Weight *= 2.2
+print(df.head(3)) # pierwsze 3 wiersze
