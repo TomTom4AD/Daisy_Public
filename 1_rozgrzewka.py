135cb96c8a48e055048895877a3c7d6194507113
TomTom4AD <tomasztomzik@yahoo.de>
2025-05-24 08:55:41 +0200

    8:55 AM

---
+++ 1_rozgrzewka.py
@@ -0,0 +1,14 @@
+# przyjmie nazwe pliku (zabezpieczenia)
+# @przyjmie rozszerzenie
+# powie czy rozszerzenie dozwolone - jesli nie to podaj inne
+# program napisze ladnie nazwe z rozszerzeniem
+
+# program wykona to dla X plikow danych razem
+
+extentions = ['txt', 'csv', 'jpg', 'mp3']
+
+file_name = input('Podaj nazwe pliku:   ')
+print(f'Podana nazwa {file_name} \na 2+2 to {2+2}')
+# len(x)
+
+ext_name = (f'Podaj rozszerzenie:   ')
