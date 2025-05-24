b1c7e441c907385b04f88939e27d759c41d981e0
TomTom4AD <tomasztomzik@yahoo.de>
2025-05-24 11:24:25 +0200

    11:25 AM

--- 2_pandas_diabetes.py
+++ 2_pandas_diabetes.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv('C:\\Users\\vdi-student\\Desktop\\diabetes.csv')
 print(df)
@@ -7,3 +8,18 @@ print(f'Kształt Danych: {df.shape}')
 print(df.describe().T.round(2).to_string()) #pokaż jak się zachowują te pola w pliku
                                             #T - transpond
                                             #round(2) - rounding
+print('Ile braków')
+print(df.isna().sum())
+
+# tam gdzie zero albo brak wartosci - dac srednia (bez zer)
+for col in ['glucose', 'bloodpressure', 'skinthickness', 'insulin', 'bmi', 'diabetespredigreefunction', 'age']:
+    df[col] = df[col].replace(0, np.nan)
+    mean_ = df[col].mean()
+    df[col].replace(np.nan, mean_, inplace=True)
+
+print('\n\n......po czyszczeniu.....\n')
+print(df.describe().T.round(2).to_string())
+print('Ile braków')
+print(df.isna().sum())
+
+df.to_csv('cukrzyca_po_obróbce.csv')
