626b9b92eceb0aa3f96860f6b0ad8dd34e3beec5
TomTom4AD <tomasztomzik@yahoo.de>
2025-05-24 09:12:59 +0200

    9:15 AM

--- 1_rozgrzewka.py
+++ 1_rozgrzewka.py
@@ -2,6 +2,7 @@
 # @przyjmie rozszerzenie
 # powie czy rozszerzenie dozwolone - jesli nie to podaj inne
 # program napisze ladnie nazwe z rozszerzeniem
+from win32pdh import counter_status_error
 
 # program wykona to dla X plikow danych razem
 
@@ -11,4 +12,15 @@ file_name = input('Podaj nazwe pliku:   ')
 print(f'Podana nazwa {file_name} \na 2+2 to {2+2}')
 # len(x)
 
-ext_name = (f'Podaj rozszerzenie:   ')
+counter_a = 0
+
+while True:
+    ext_name = input(f'Podaj rozszerzenie:   ')
+    if ext_name in extentions:
+        print(f'zaakceptowano {ext_name}')
+        break
+    else:
+        print('niepoprawne rozszerzenie')
+        counter_a = counter_a + 1   # counter_a += 1 to to samo
+        if counter_a == 3:
+            break
